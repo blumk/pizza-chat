a930033b623958b4c600cd4ec1c16929c84bea47
Ken Blum <ken.blum@bbv.ch>
2019-08-30 13:04:05 +0200

    pizza chat init

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { RouteComponentProps } from 'react-router';
-import { IonContent, IonHeader, IonItem, IonLabel, IonList, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonItem, IonLabel, IonList, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent } from '@ionic/react';
 
 const Tab2: React.FunctionComponent<RouteComponentProps> = ({ history }) => {
 
@@ -8,10 +8,25 @@ const Tab2: React.FunctionComponent<RouteComponentProps> = ({ history }) => {
     <>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab Two</IonTitle>
+          <IonTitle>Pizzas</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
+
+      <IonCard className="welcome-card">
+          <img src="/assets/shapes.svg" alt=""/>
+          <IonCardHeader>
+            <IonCardSubtitle>Get Started</IonCardSubtitle>
+            <IonCardTitle>Welcome to Ionic</IonCardTitle>
+          </IonCardHeader>
+          <IonCardContent>
+            <p>
+              Now that your app has been created, you'll want to start building out features and
+              components. Check out some of the resources below for next steps.
+            </p>
+          </IonCardContent>
+        </IonCard>
+        
         <IonList>
           <IonItem href="/tab2/details">
             <IonLabel>
--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,15 +1,38 @@
 import React from 'react';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/react';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonListHeader, IonLabel, IonItem, IonIcon } from '@ionic/react';
+import { book, build, grid, colorFill } from 'ionicons/icons';
 
 const Tab3Page: React.FunctionComponent = () => {
   return (
     <>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab Three</IonTitle>
+          <IonTitle>Settings</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent />
+      <IonContent>
+      <IonList lines="none">
+          <IonListHeader>
+            <IonLabel>Resources</IonLabel>
+          </IonListHeader>
+          <IonItem href="https://ionicframework.com/docs/" target="_blank">
+            <IonIcon slot="start" color="medium" icon={book} />
+            <IonLabel>Ionic Documentation</IonLabel>
+          </IonItem>
+          <IonItem href="https://ionicframework.com/docs/building/scaffolding" target="_blank">
+            <IonIcon slot="start" color="medium" icon={build} />
+            <IonLabel>Scaffold Out Your App</IonLabel>
+          </IonItem>
+          <IonItem href="https://ionicframework.com/docs/layout/structure" target="_blank">
+            <IonIcon slot="start" color="medium" icon={grid} />
+            <IonLabel>Change Your App Layout</IonLabel>
+          </IonItem>
+          <IonItem href="https://ionicframework.com/docs/theming/basics" target="_blank">
+            <IonIcon slot="start" color="medium" icon={colorFill} />
+            <IonLabel>Theme Your App</IonLabel>
+          </IonItem>
+        </IonList>
+      </IonContent>
     </>
   );
 };
